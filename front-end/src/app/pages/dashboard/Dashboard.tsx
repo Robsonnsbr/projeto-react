7645ef3a40c9b2ca002410ea14a3c128c6a70487
Robsonnsbr <robsonnsbr@gmail.com>
2023-03-10 20:28:10 -0300

    feat: add login page end form

--- front-end/src/app/pages/dashboard/Dashboard.tsx
+++ front-end/src/app/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Dashboard = () => {
-    return <p>Dashboard</p>;
+    return (
+        <div style={{ color: "red" }}>
+            <p>Dashboard</p>
+            <Link to={"/entrar"}>Login</Link>
+        </div>
+    );
 };
--- front-end/src/app/pages/login/Login.tsx
+++ front-end/src/app/pages/login/Login.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+
 export const Login = () => {
-    return <p>LOGIN</p>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEnter = () => {
+        console.log(email);
+        console.log(password);
+    };
+
+    return (
+        <div>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <button type="button" onClick={handleEnter}>
+                    Entrar{" "}
+                </button>
+            </form>
+        </div>
+    );
 };
