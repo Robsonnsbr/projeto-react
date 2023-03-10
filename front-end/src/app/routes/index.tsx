7a30566a976ce3baaba75702498cb4327074aa2c
Robsonnsbr <robsonnsbr@gmail.com>
2023-03-09 23:48:55 -0300

    feat: adicionando componentes

--- front-end/src/app/routes/index.tsx
+++ front-end/src/app/routes/index.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Route, Navigate } from "react-router-dom";
 import { Routes as Switch } from "react-router-dom";
-
-// import { Dashboard, Login } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/entrar" element={<h1>Olá</h1>} />
-                <Route path="/pagina-inicial" element={<h1>Olá</h1>} />
+                <Route path="/entrar" element={<Login />} />
+                <Route path="/pagina-inicial" element={<Dashboard />} />
 
-                <Route path="/" element={<Navigate to="pagina-inicial" />} />
+                <Route path="*" element={<Navigate to="pagina-inicial" />} />
             </Switch>
         </BrowserRouter>
     );
