a4f79a2faa931034621c869066d50184d4465a8e
Robsonnsbr <robsonnsbr@gmail.com>
2023-03-12 00:21:56 -0300

    feat: include functions

--- front-end/src/app/pages/login/Login.tsx
+++ front-end/src/app/pages/login/Login.tsx
@@ -1,35 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState, useCallback, useRef } from "react";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
     const [email, setEmail] = useState(""); //TODO: precisamos usar o useState para renderizar o componente em tela
     const [password, setPassword] = useState("");
-
-    const handleEnter = () => {
-        console.log(email);
-        console.log(password);
-    };
-
-    useEffect(() => {
-        console.log(email);
+    const emaillength = useMemo(() => {
+        return email.length * 1000;
     }, [email]);
 
-    useEffect(() => {
+    const handleEnter = useCallback(() => {
+        console.log(email);
         console.log(password);
-    }, [password]);
+    }, [email, password]);
+    // useEffect(() => {
+    //     console.log(email);
+    // }, [email]);
+
+    // useEffect(() => {
+    //     console.log(password);
+    // }, [password]);
 
     return (
         <div>
             <form>
                 <label>
+                    <p>Quantidade de caracteres email: {emaillength}</p>
                     <span>Email</span>
                     <input
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        onKeyDown={(e) =>
+                            e.key === "Enter"
+                                ? inputPasswordRef.current?.focus()
+                                : undefined
+                        }
                     />
                 </label>
                 <label>
                     <span>Senha</span>
                     <input
+                        ref={inputPasswordRef}
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
