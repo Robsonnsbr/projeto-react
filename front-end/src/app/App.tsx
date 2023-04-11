881bf19acc34778c4028882c30bc71159fe38ff1
robsonnsbr@gmail.com <robsonnsbr@gmail.com>
2023-04-11 11:41:55 -0300

    feat: deply

--- front-end/src/app/App.tsx
+++ front-end/src/app/App.tsx
@@ -4,7 +4,6 @@ import { UsuarioLogadoProvider } from './shared/contexts';
 export const App = () => {
   return (
     <UsuarioLogadoProvider>
-      <style type="text/css"></style>
       <Routes />
     </UsuarioLogadoProvider>
   );
